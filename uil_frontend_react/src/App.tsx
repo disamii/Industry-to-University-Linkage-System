399427e7cf70cd63958c478e22223dd53f151962
sisk1 <siisaykewiti@gmail.com>
2026-04-25 16:21:36 +0300

    initial commit for react app

---
+++ uil_frontend_react/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="font-black text-3xl">Hello World</h1>
+      <Button>THIS</Button>
+    </div>
+  );
+}
+
+export default App;
